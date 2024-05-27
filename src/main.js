54baef185b7587c1e3301db092feebbbbaeca31a
santu <j6458221@yahoo.com.tw>
2024-05-27 13:36:51 +0800

    手機驗證

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+// import './assets/main.css'
+import './assets/all.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,7 +7,31 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 匯入 vee-validate 多國語系的功能
+import { localize, setLocale } from '@vee-validate/i18n'
+// 匯入 vee-validate 繁體中文語系檔案
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+// 匯入 vee-validate 相關規則
+import * as rules from '@vee-validate/rules'
+// 匯入 vee-validate 主套件
+import { configure, defineRule, ErrorMessage, Field, Form } from 'vee-validate'
+// vee-validate 定義驗證規則(規則啟用)
+Object.keys(rules).forEach((rule) => {
+  defineRule(rule, rules[rule])
+})
+// vee-validate 全域規則、多國語系
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true // 一輸入值直接進行驗證
+})
+// 設定預設語系 (如果上方設定很多語系，可以在此加入預設語系)
+setLocale('zh_TW')
+
 const app = createApp(App)
+
+app.component('VForm', Form)
+app.component('VField', Field)
+app.component('ErrorMessage', ErrorMessage)
 
 app.use(createPinia())
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,56 @@
 <template>
-  <div class="home">
-    <h1>Hello, This is Home Page.</h1>
-    <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/about">About</RouterLink>
-    <hr>
-    <HelloWorld />
+  <div class="">
+    <img src="https://noke.jutretail.com.tw/images/logo.png" alt="logo">
+  </div>
+  <div class="container">
+    <img class="w-100 d-block" src="../../public/母親節DM_20240417-完稿6.jpg" alt="">
+    <VForm v-slot="{ errors }" @submit="handleOrderSubmit()">
+      <div class="col-md-4 mb-2 mx-auto">
+        <label for="orderTel" class="sr-only text-primary py-3"
+          ><span class="text-danger me-1 align-middle">*</span>手機號碼</label
+        >
+        <VField
+          type="text"
+          id="orderTel"
+          name="電話"
+          class="form-control"
+          :class="{ 'is-invalid': errors['電話'] }"
+          placeholder="請輸入電話"
+          rules="required|numeric|min:10|max:10"
+          autofocus
+          v-model="userData.data.user.tel"
+        ></VField>
+        <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+      </div>
+      <div class="col-md-4 mb-4 mx-auto">
+        <span class="text-danger me-1 align-middle">*</span>
+        <VField  name="terms" type="checkbox" :value="true" :unchecked-value="false" :class="{ 'is-invalid': errors['terms'] }" rules="required" autofocus>
+            <label>
+              <input type="checkbox" name="terms" v-bind="field" :value="true"/>
+              我同意
+            </label>
+        </VField>
+        <ErrorMessage name="terms" class="invalid-feedback"></ErrorMessage>
+      </div>
+      <button class="mx-auto d-block mb-6">下一步</button>
+    </VForm>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  components: {
-    HelloWorld
+  data () {
+    return {
+      userData: {
+        data: {
+          user: {
+            tel: ''
+          }
+        }
+      }
+    }
   }
 }
 </script>
