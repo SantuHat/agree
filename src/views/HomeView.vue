8530c2c48711c76add289e0e829cae147dccefed
santu <j6458221@yahoo.com.tw>
2024-06-05 17:42:36 +0800

    加入設計稿樣式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,44 @@
 <template>
-  <RouterLink to="/">
+  <!-- <RouterLink to="/">
     <img src="https://noke.jutretail.com.tw/images/logo.png" alt="logo">
-  </RouterLink>
-  <div class="container">
-    <img class="w-100 d-block" src="../../public/0329-_08-美妝.jpg" alt="">
+  </RouterLink> -->
+  <div class=" d-lg-none position-relative">
 
-    <RouterLink to="/registrate-finished" class="form-group text-center mb-5">
-      <button type="button" class="btn btn-lg border-white btn-primary my-5 d-block mx-auto">立即登錄</button>
+    <img class="w-100" src="../../public/noke-0607-0708.jpg" alt="noke-img">
+    <RouterLink to="/registrate-finished" class="form-group text-center z_index btn_position">
+      <button type="button" class="btn  btn_size"></button>
     </RouterLink>
-
+    <!-- noke -->
+    <a class="noke_position" href="https://noke.jutretail.com.tw/">
+      <img class="noke_size" src="../../public/noke-icon.png" alt="">
+    </a>
+    <!-- fb -->
+    <a class="fb_position" href="https://noke.jutretail.com.tw/">
+      <img class="noke_size" src="../../public/fb-icon.png" alt="">
+    </a>
+    <!-- ig -->
+    <a class="ig_position" href="https://noke.jutretail.com.tw/">
+      <img class="noke_size" src="../../public/ig-icon.png" alt="">
+    </a>
+  </div>
+  <!-- desktop -->
+  <div class="container d-none d-lg-block">
+    <img class="w-100" src="../../public/noke-0607-0708.jpg" alt="noke-img">
+    <RouterLink to="/registrate-finished" class="form-group text-center z_index btn_position">
+      <button type="button" class="btn  btn_size"></button>
+    </RouterLink>
+    <!-- noke -->
+    <a class="noke_position" href="https://noke.jutretail.com.tw/">
+      <img class="noke_size" src="../../public/noke-icon.png" alt="">
+    </a>
+    <!-- fb -->
+    <a class="fb_position" href="https://noke.jutretail.com.tw/">
+      <img class="noke_size" src="../../public/fb-icon.png" alt="">
+    </a>
+    <!-- ig -->
+    <a class="ig_position" href="https://noke.jutretail.com.tw/">
+      <img class="noke_size" src="../../public/ig-icon.png" alt="">
+    </a>
   </div>
 </template>
 
@@ -27,6 +57,139 @@ export default {
   }
 }
 </script>
-
 <style scoped>
+.bgImg {
+  background-image: url(noke-0607-0708.jpg);
+  /* height: 2000px; */
+  /* background-repeat: no-repeat; */
+  background-size: cover;
+  background-position: center;
+}
+.z_index {
+  z-index: 1;
+}
+.btn_position {
+  position: absolute;
+  bottom: 8%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.btn_size {
+  width: 200px;
+  height: 80px;
+}
+.noke_position {
+  position: absolute;
+  bottom: 4.5%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.fb_position {
+  position: absolute;
+  bottom: -1%;
+  left: 24%;
+  transform: translate(-50%, -50%);
+}
+.ig_position {
+  position: absolute;
+  bottom: -1%;
+  left: 76%;
+  transform: translate(-50%, -50%);
+}
+.noke_size {
+  width: 40px;
+  /* height: 20px; */
+}
+@media (min-width: 700px) {
+  .btn_position {
+    bottom: 9%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .btn_size {
+    width: 360px;
+    height: 130px;
+  }
+}
+@media (min-width: 992px) {
+  .btn_position {
+    bottom: -335%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .noke_position {
+    bottom: -358%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .fb_position {
+    bottom: -383%;
+    left: 24%;
+    transform: translate(-50%, -50%);
+  }
+  .ig_position {
+    bottom: -383%;
+    left: 76%;
+    transform: translate(-50%, -50%);
+  }
+  .btn_size {
+    width: 420px;
+    height: 150px;
+  }
+  .noke_size {
+    width: 100px;
+  }
+}
+@media (min-width: 1200px) {
+  .btn_position {
+    bottom: -420%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .btn_size {
+    width: 500px;
+    height: 180px;
+  }
+  .noke_position {
+    bottom: -445%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .fb_position {
+    bottom: -475%;
+    left: 26%;
+    transform: translate(-50%, -50%);
+  }
+  .ig_position {
+    bottom: -475%;
+    left: 74%;
+    transform: translate(-50%, -50%);
+  }
+}
+@media (min-width: 1400px) {
+  .btn_position {
+    bottom: -505%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .btn_size {
+    width: 600px;
+    height: 220px;
+  }
+  .noke_position {
+    bottom: -532%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .fb_position {
+    bottom: -564%;
+    left: 26%;
+    transform: translate(-50%, -50%);
+  }
+  .ig_position {
+    bottom: -564%;
+    left: 74%;
+    transform: translate(-50%, -50%);
+  }
+}
 </style>
--- src/views/RegistrateView.vue
+++ src/views/RegistrateView.vue
@@ -1,54 +1,88 @@
 <template>
-  <RouterLink to="/">
-    <img src="https://noke.jutretail.com.tw/images/logo.png" alt="logo">
-  </RouterLink>
-  <div class="container">
-    <div class="form-group text-center mb-5" v-if="isRegistration">
-      <h3 class="fw-bold mt-5 text-blue">您先前已報名成功!</h3>
-      <img class="w-100 d-block" src="../../public/母親節DM_20240417-完稿6.jpg" alt="">
-      <button class="btn btn-primary my-4 d-block mx-auto border-white" @click="updateInput('update'), changeTitle = true">更換其他號碼參加活動</button>
-      <button @click="goLuckyPage" type="button" class="btn btn-secondary my-4 d-block mx-auto ">查詢抽獎號嗎</button>
-    </div>
-    <VForm v-slot="{ errors }" @submit="handleOrderSubmit($event)"  v-else>
-      <div class="col-md-4 mb-2 mx-auto">
-        <label for="orderTel" class="sr-only text-blue fw-bold d-block py-3 text-center fs-2"
-          >
-          <!-- <span class="text-danger me-1 align-middle">
-            *
-          </span> -->
-          {{ changeTitle? '更換手機號碼': '登錄手機號碼抽獎' }}
-          </label
-        >
-        <VField
-        type="tel"
-        id="orderTel"
-          name="手機號碼"
-          class="form-control bg-white"
-          :class="{ 'is-invalid': errors['手機號碼'] }"
-          placeholder="請輸入手機號碼"
-          rules="required|numeric|min:10|max:10|startsWith09"
-          v-model="userData.tel"
-          ></VField>
-          <ErrorMessage name="手機號碼" class="invalid-feedback"></ErrorMessage>
-        </div>
-        <!-- img -->
+  <div class="bg-151515">
+    <div class="container">
+      <div class="form-group text-center mb-5" v-if="isRegistration">
+        <h3 class="fw-bold mt-5 text-blue">您先前已報名成功!</h3>
         <img class="w-100 d-block" src="../../public/母親節DM_20240417-完稿6.jpg" alt="">
-        <div class="col-md-4 mb-2 mx-auto" v-if="type === 'create'">
-          <div class="d-flex" :class="{ 'is-invalid': errors.acceptTerms }">
-            <!-- <span class="text-danger me-1 align-middle">*</span> -->
-            <div class="form-group form-check">
-              <VField name="acceptTerms" type="checkbox" id="acceptTerms" :value="true" class="form-check-input border-white"  rules="required" />
-              <label for="acceptTerms" class="form-check-label text-white">已閱讀以上注意事項並同意參加</label>
-            </div>
-          </div>
-          <div  class="invalid-feedback">{{errors.acceptTerms ? '請勾選同意': ''}}</div>
-        </div>
-      <div class="form-group text-center my-5 d-flex justify-content-evenly">
-        <button type="submit" class="btn btn-primary mr-1 border-white">{{ type === 'create' ? '參加活動' : '送出'}}</button>
-        <button v-if="type === 'update'" @click="this.isRegistration = true" type="button" class="btn btn-secondary">返回</button>
+        <button class="btn btn-primary my-4 d-block mx-auto border-white" @click="updateInput('update'), changeTitle = true">更換其他號碼參加活動</button>
+        <button @click="goLuckyPage" type="button" class="btn btn-secondary my-4 d-block mx-auto ">查詢抽獎號嗎</button>
       </div>
-    </VForm>
+      <VForm v-slot="{ errors }" @submit="handleOrderSubmit($event)"  v-else>
+        <div class="col-md-4 mb-2 mx-auto">
+          <label for="orderTel" class="sr-only text-primary fw-bold d-block py-4 text-center fs-1"
+            >
+            <!-- <span class="text-danger me-1 align-middle">
+              *
+            </span> -->
+            {{ changeTitle? '更換手機號碼': '登錄手機號碼抽獎' }}
+            </label
+          >
+          <VField
+          type="tel"
+          id="orderTel"
+            name="手機號碼"
+            class="form-control bg-white "
+            :class="{ 'is-invalid': errors['手機號碼'] }"
+            placeholder="請輸入手機號碼"
+            rules="required|numeric|min:10|max:10|startsWith09"
+            v-model="userData.tel"
+            ></VField>
+            <ErrorMessage name="手機號碼" class="invalid-feedback"></ErrorMessage>
+          </div>
+          <div class="col-lg-5 mx-auto">
+            <img class="w-100 mb-1" src="../../public/65311.jpg" alt="">
+          </div>
 
+          <!-- <div class="text-white noticeFont col-md-6 mx-auto">
+            <p class="mb-0">活動注意事項：</P>
+            <p class="mb-0">●參加本活動抽獎者，必須為JUT LIFE忠泰生活圈(以下簡稱JUT APP)</p>
+            <p class="mb-0">會員並於消費時限完成活動頁面登錄消費發票號碼門檻方具有抽獎資格。</p>
+            <p class="mb-0">●會員於實體消費結帳前，須「主動出示 JUT APP會員條碼」始符合抽獎資格。</p>
+            <p class="mb-0">● JUT APP會員於活動時間內至NOKE全館指定餐飲品牌消費，單筆不限</p>
+            <p class="mb-0">金額消費，即獲得1次抽獎資格，兩筆不限金額消費，即獲得2次抽獎資格，以此類推；</p>
+            <p class="mb-0">抽獎資格每會員每日無上限。</p>
+            <p class="mb-0">將以中獎知該筆消費金額等值NOKE中泰勒生活-電子商品禮券回饋中</p>
+            <p class="mb-0">獎者，單筆免單回饋上限1,111元。</p>
+            <p class="mb-0">●下列發票及消費恕不列入計算：商品券、贈品禮券、電子商品禮券、電子</p>
+            <p class="mb-0">贈品券、JUT Points折抵、提貨單、二三聯式手開發票、購買及加值儲值卡、</p>
+            <p class="mb-0">未開立NOKE忠泰樂生活發票之交易，SMART PAY金融卡交易、LINE PAY、</p>
+            <p class="mb-0">NFC/HCE手機信用卡等消費金額。</p>
+            <p class="mb-0">●各波段會員消費發票補登須於抽獎日期前一日23:59前且完成登錄動作</p>
+              <p>方符合該期抽獎資格；抽獎截止日前若退貨恕不符合活動資格將取消抽獎</p>
+              <p>資格；登錄成功僅表示參與本次活動意願，抽獎資格仍應依活動辦法為之，</p>
+              <p>若資格不符將取消抽獎資格。</p>
+            <p class="mb-0">●將於各波段抽獎日期抽出中獎者並公告於活動網頁，系統將自動派送電</p>
+            <p class="mb-0">子商品禮券。各波段中獎者經消費資格驗證後預計於2024/07/19前統一歸</p>
+            <p class="mb-0">戶，活動獲得之電子商品禮券限使用至2024/12/31止；逾期未兌換者視同</p>
+            <p class="mb-0">放棄。請參閱NOKE忠泰樂生活-電子商品禮券使用規範與注意事項</p>
+            <p class="mb-0"><a href="https://reurl.cc/zlq3Dk">https://reurl.cc/zlq3Dk</a></p>
+            <p class="mb-0">●中獎資格及權利不得轉讓，並不得兌換現金、抵換其他贈品。</p>
+            <p class="mb-0">●中獎稅依財政部國稅局【所得稅法】辦理，若得獎人為未成年人，則由法</p>
+            <p class="mb-0">定代理人代領，贈品列入代領人所得申報。</p>
+            <p class="mb-0">●為維護參加者權益及辦理活動之公平性，本活動參加者若透過系統漏洞</p>
+            <p class="mb-0">或非法手段(異常帳號建立、惡意程式等類似行為) 參與本活動，將取消其</p>
+            <p class="mb-0">資格，並視情節限制該帳號當下與日後活動參加權利。</p>
+            <p class="mb-0">●相關注意事項如有更改，以現場公告及說明為準，參加者同意主辦單位</p>
+            <p class="mb-0">得以網站活動公告後調整修改之方式取代通知，並有權對本活動所有事宜</p>
+            <p class="mb-0">作出解釋或裁決。</p>
+          </div> -->
+
+          <div class="col-md-4 mb-2 mx-auto" v-if="type === 'create'">
+            <div class="d-flex" :class="{ 'is-invalid': errors.acceptTerms }">
+              <!-- <span class="text-danger me-1 align-middle">*</span> -->
+              <div class="form-group form-check mx-auto">
+                <VField name="acceptTerms" type="checkbox" id="acceptTerms" :value="true" class="form-check-input "  rules="required" />
+                <label for="acceptTerms" class="form-check-label text-white noticeFont">已閱讀以上注意事項並同意參加</label>
+              </div>
+            </div>
+            <div  class="invalid-feedback text-center">{{errors.acceptTerms ? '請勾選同意': ''}}</div>
+          </div>
+        <div class="form-group text-center mt-5 pb-5 d-flex justify-content-evenly">
+          <button type="submit" class="btn btn-primary mr-1 border-white">{{ type === 'create' ? '參加活動' : '送出'}}</button>
+          <button v-if="type === 'update'" @click="this.isRegistration = true" type="button" class="btn btn-secondary">返回</button>
+        </div>
+      </VForm>
+    </div>
   </div>
 </template>
 
@@ -159,4 +193,10 @@ export default {
 </script>
 
 <style scoped>
+.bg-151515 {
+  background-color: #151515;
+}
+.noticeFont {
+  font-size: 14px;
+}
 </style>
