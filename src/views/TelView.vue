28025e0b0763b8d6ea5315d5cddef33726f01ed8
santu <j6458221@yahoo.com.tw>
2024-06-13 10:11:49 +0800

    tel&render data

--- src/views/TelView.vue
+++ src/views/TelView.vue
@@ -1,5 +1,39 @@
 <template>
-  <RouterLink to="/">Home</RouterLink> |
+  <!-- <RouterLink to="/">Home</RouterLink> | -->
+  <div class="d-flex">
+    <!-- <section class="">
+      <button type="button" @click="filterData('2024-06-12')">
+        6/12
+      </button>
+      <h5>人數:{{ filteredData.length }}</h5>
+    </section> -->
+    <section class="">
+      <button type="button" @click="filterData('2024-06-13')">
+        6/13
+      </button>
+      <h5>人數:{{ filteredData.length }}</h5>
+    </section>
+  </div>
+  <!-- <pre>
+    {{ filteredData }}
+  </pre> -->
+    <table class="text-center box-shadow-gray-300 border-collapse border-1 p-1" >
+      <thead>
+        <tr>
+          <th>編號</th>
+          <th>手機號碼</th>
+          <th>建立時間</th>
+        </tr>
+      </thead>
+      <tbody v-for="item in filteredData" :key="item.id">
+        <tr>
+          <td>{{ item.id }}</td>
+          <td class="p-1">{{ item.tel }}</td>
+          <td class="p-1">{{ item.created_at }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <button type="button">All</button>
     <table class="text-center box-shadow-gray-300 border-collapse border-1 p-1" >
       <!-- <caption>
         報名資訊 2024
@@ -29,7 +63,8 @@ const { VITE_APP_API_URL, VITE_APP_API_NAME } = import.meta.env
 export default {
   data () {
     return {
-      telData: []
+      telData: [],
+      filteredData: []
     }
   },
   methods: {
@@ -43,6 +78,11 @@ export default {
         .catch((err) => {
           console.log(err)
         })
+    },
+    filterData (date) {
+      this.filteredData = this.telData.filter((item) => {
+        return item.created_at.startsWith(date)
+      })
     }
   },
   mounted () {
