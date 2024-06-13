ff5fe4c804d9c3dc8f9b2acb07b3170fbdc4720a
santu <j6458221@yahoo.com.tw>
2024-06-13 19:00:17 +0800

    新增四波名單

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,43 +2,28 @@
   <!-- <RouterLink to="/">
     <img src="https://noke.jutretail.com.tw/images/logo.png" alt="logo">
   </RouterLink> -->
-  <div class=" d-lg-none position-relative">
+  <div class="bgImg">
+    <div class="h_section"></div>
+    <button type="button" class="login_btn mx-auto mb-4">
+      立即登錄
+    </button>
+    <div class="container border-bottom border-top border-white mb-2">
+      <div class="d-flex justify-content-between w-350 mx-auto">
+        <a class="d-block text-white p-2 border-end border-white fw-bold" href="https://docs.google.com/document/d/1Gl2vhXCM59CL4Oa3LB8k8_gmhqfebkPm0W6e_A5XUxk/edit?usp=sharing">第一波<br>中獎名單<br>6.7-6.14</a>
+        <a class="d-block text-white p-2 border-end border-white ps-0 fw-bold" href="https://docs.google.com/document/d/17TZAyDTAs52gw4tC1BxihXtTTqGke2ctvA8vEVnSIq4/edit?usp=sharing">第二波<br>中獎名單<br>6.15-6.22</a>
+        <a class="d-block text-white p-2 border-end border-white ps-0 fw-bold" href="https://docs.google.com/document/d/1z8QVtuUOYl8YFNn2HxtWggA2wuCVRfREGtaN6YSL9G4/edit?usp=sharing">第三波<br>中獎名單<br>6.23-6.30</a>
+        <a class="d-block text-white p-2 ps-0 fw-bold" href="https://docs.google.com/document/d/187FyGgg4j7VvEPK7naPaqDKwOlim8x4gQ3sUT0w-5es/edit?usp=sharing">第四波<br>中獎名單<br>7.1-7.8</a>
+      </div>
+    </div>
+    <div class="d-flex justify-content-evenly w-350 mx-auto">
+      <a class="d-block" href="http://pili.app/fb/SQ8QIutF">
+        <img class="logo_size" src="../../public/fb-icon.png" alt="">
+      </a>
+      <a href="https://www.instagram.com/nokemall/">
+        <img class="logo_size" src="../../public/ig-icon.png" alt="">
+      </a>
+    </div>
 
-    <img class="w-100" src="../../public/login.jpg" alt="noke-img">
-    <RouterLink to="/registrate-finished" class="form-group text-center z_index btn_position">
-      <button type="button" class="btn  btn_size"></button>
-    </RouterLink>
-    <!-- noke -->
-    <a class="noke_position" href="https://reurl.cc/3X9k1V">
-      <P class="text-white fw-bold">中獎查詢</P>
-    </a>
-    <!-- fb -->
-    <a class="fb_position" href="http://pili.app/fb/SQ8QIutF">
-      <img class="noke_size" src="../../public/fb-icon.png" alt="fb-icon">
-    </a>
-    <!-- ig -->
-    <a class="ig_position" href="https://www.instagram.com/nokemall/">
-      <img class="noke_size" src="../../public/ig-icon.png" alt="ig-icon">
-    </a>
-  </div>
-  <!-- desktop -->
-  <div class="container d-none d-lg-block">
-    <img class="w-100" src="../../public/login.jpg" alt="noke-img">
-    <RouterLink to="/registrate-finished" class="form-group text-center z_index btn_position">
-      <button type="button" class="btn  btn_size"></button>
-    </RouterLink>
-    <!-- noke -->
-    <a class="noke_position" href="https://reurl.cc/3X9k1V">
-      <P class="text-white fw-bold fs-4">中獎查詢</P>
-    </a>
-    <!-- fb -->
-    <a class="fb_position" href="http://pili.app/fb/SQ8QIutF">
-      <img class="noke_size" src="../../public/fb-icon.png" alt="fb-icon">
-    </a>
-    <!-- ig -->
-    <a class="ig_position" href="https://www.instagram.com/nokemall/">
-      <img class="noke_size" src="../../public/ig-icon.png" alt="ig-icon">
-    </a>
   </div>
 </template>
 
@@ -59,137 +44,31 @@ export default {
 </script>
 <style scoped>
 .bgImg {
-  background-image: url(noke-0607-0708.jpg);
-  /* height: 2000px; */
-  /* background-repeat: no-repeat; */
-  background-size: cover;
+  background-image: url(0613_index.jpg);
+  height: 1180px;
+  background-repeat: no-repeat;
+  background-size: contain;
   background-position: center;
+  /* height: 1600px; */
 }
-.z_index {
-  z-index: 1;
-}
-.btn_position {
-  position: absolute;
-  bottom: 8%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-.btn_size {
+.login_btn {
+  background: none;
+  border-color: white;
+  border-radius: 28px;
   width: 200px;
-  height: 80px;
+  height: 60px;
+  color: white;
+  font-size: 40px;
+  display: block;
 }
-.noke_position {
-  position: absolute;
-  bottom: 2.5%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.h_section {
+  height: 970px;
 }
-.fb_position {
-  position: absolute;
-  bottom: -1%;
-  left: 24%;
-  transform: translate(-50%, -50%);
+.w-350 {
+  max-width: 340px;
 }
-.ig_position {
-  position: absolute;
-  bottom: -1%;
-  left: 76%;
-  transform: translate(-50%, -50%);
-}
-.noke_size {
-  width: 40px;
-  /* height: 20px; */
-}
-@media (min-width: 700px) {
-  .btn_position {
-    bottom: 9%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .btn_size {
-    width: 360px;
-    height: 130px;
-  }
-}
-@media (min-width: 992px) {
-  .btn_position {
-    bottom: -335%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .noke_position {
-    bottom: -358%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .fb_position {
-    bottom: -383%;
-    left: 24%;
-    transform: translate(-50%, -50%);
-  }
-  .ig_position {
-    bottom: -383%;
-    left: 76%;
-    transform: translate(-50%, -50%);
-  }
-  .btn_size {
-    width: 420px;
-    height: 150px;
-  }
-  .noke_size {
-    width: 100px;
-  }
-}
-@media (min-width: 1200px) {
-  .btn_position {
-    bottom: -420%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .btn_size {
-    width: 500px;
-    height: 180px;
-  }
-  .noke_position {
-    bottom: -445%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .fb_position {
-    bottom: -475%;
-    left: 26%;
-    transform: translate(-50%, -50%);
-  }
-  .ig_position {
-    bottom: -475%;
-    left: 74%;
-    transform: translate(-50%, -50%);
-  }
-}
-@media (min-width: 1400px) {
-  .btn_position {
-    bottom: -505%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .btn_size {
-    width: 600px;
-    height: 220px;
-  }
-  .noke_position {
-    bottom: -532%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .fb_position {
-    bottom: -564%;
-    left: 26%;
-    transform: translate(-50%, -50%);
-  }
-  .ig_position {
-    bottom: -564%;
-    left: 74%;
-    transform: translate(-50%, -50%);
-  }
+.logo_size {
+  width: 30px;
+  display: block;
 }
 </style>
